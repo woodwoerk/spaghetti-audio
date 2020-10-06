20541aec6d238af8c66f63cf918cf425746ddce6
Joe Smallwood <joeharrysmallwood@gmail.com>
2020-10-06 17:19:29 +0300

    Update build config for npm

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,15 @@ const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
+  mode: 'production',
   target: 'web',
+  devtool: false,
   entry: './src/index.ts',
+  output: {
+    libraryExport: 'default',
+  },
+  performance: {
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
 })
